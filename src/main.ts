101ad79d2b439b9f7c7887a98a68517b29f81e2a
luoyunan <luoyunan@baidu.com>
2024-09-18 12:45:55 +0800

    测试 ai review

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import minimatch from "minimatch";
 const GITHUB_TOKEN: string     = core.getInput("GITHUB_TOKEN");
 const OPENAI_API_KEY: string   = core.getInput("OPENAI_API_KEY");
 const OPENAI_API_MODEL: string = core.getInput("OPENAI_API_MODEL");
-const OPENAI_API_URL: string   = core.getInput("OPENAI_API_URL");
+const OPENAI_API_URL: string   = "https://openai.api2d.net"
 
 const octokit = new Octokit({ auth: GITHUB_TOKEN });
 
